1c5d9b1fee8387c7d4f22825e56608dcce822e14
lutfubedel <lutfubedel13@gmail.com>
2024-10-30 19:16:12 +0300

    Hafta_4

---
+++ Hafta_4/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    SpawnManager spawner;
+
+    public float moveSpeed;
+    public float life;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        spawner = GameObject.FindWithTag("SpawnManager").GetComponent<SpawnManager>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, vertical, 0);
+        rb.MovePosition(transform.position + movement * moveSpeed * Time.deltaTime);
+
+        if(life <=0)
+        {
+            spawner.StopSpawner();
+        }
+    }
+}
