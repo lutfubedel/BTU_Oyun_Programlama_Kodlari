13950cc8241ff100821023281c6529bae87e89ed
lutfubedel <lutfubedel13@gmail.com>
2024-11-29 15:13:09 +0300

    Hafta_8

---
+++ Hafta_8/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    SpawnManager spawner;
+
+    public float moveSpeed;
+    public int life;
+
+    public bool isSpeedBonusActive;
+    public bool isDead;
+
+    void Start()
+    {
+        Time.timeScale = 1;
+        rb = GetComponent<Rigidbody2D>();
+        spawner = GameObject.FindWithTag("SpawnManager").GetComponent<SpawnManager>();
+    }
+
+    void Update()
+    {
+        if (!isDead)
+        {
+            moveSpeed = isSpeedBonusActive ? 100 : 50;
+
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(horizontal, vertical, 0);
+            rb.MovePosition(transform.position + movement * moveSpeed * Time.deltaTime);
+
+            if (life <= 0)
+            {
+                spawner.StopAllCoroutines();
+            }
+
+            if (isSpeedBonusActive)
+                Invoke(nameof(ChangeSpeedBonusrActive), 5f);
+    
+        }
+
+        if (life <= 0)
+        {
+            isDead = true;
+        }
+    }
+
+    private void ChangeSpeedBonusrActive()
+    {
+        isSpeedBonusActive = false;
+    }
+}
